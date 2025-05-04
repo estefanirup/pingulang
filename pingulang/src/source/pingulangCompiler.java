2cec83400819c778a3faf2c8d416cadda458d995
estefanirup <estefaniaparecidaruproza@gmail.com>
2025-05-04 19:18:18 -0300

    Atualizacao do for amem

--- pingulang/src/source/pingulangCompiler.java
+++ pingulang/src/source/pingulangCompiler.java
@@ -273,6 +273,7 @@ public class pingulangCompiler implements pingulangCompilerConstants {
         }
       case ID:{
         Atribuicao();
+        jj_consume_token(PINGU);
         break;
         }
       case PRINT:
@@ -336,7 +337,6 @@ public class pingulangCompiler implements pingulangCompilerConstants {
       jj_consume_token(-1);
       throw new ParseException();
     }
-    jj_consume_token(PINGU);
 }
 
 /* ESTRUTURAS CONDICIONAIS */
@@ -599,39 +599,45 @@ public class pingulangCompiler implements pingulangCompilerConstants {
     finally { jj_save(2, xla); }
   }
 
-  static private boolean jj_3R_Condicional_182_5_8()
+  static private boolean jj_3R_Condicional_181_5_8()
  {
     if (jj_scan_token(IF)) return true;
     if (jj_scan_token(36)) return true;
     return false;
   }
 
-  static private boolean jj_3R_EntradaSaida_225_7_22()
+  static private boolean jj_3R_EntradaSaida_224_7_22()
  {
     if (jj_scan_token(INPUT)) return true;
     return false;
   }
 
-  static private boolean jj_3R_EntradaSaida_224_5_16()
+  static private boolean jj_3R_EntradaSaida_223_5_16()
  {
     Token xsp;
     xsp = jj_scanpos;
-    if (jj_3R_EntradaSaida_224_5_21()) {
+    if (jj_3R_EntradaSaida_223_5_21()) {
     jj_scanpos = xsp;
-    if (jj_3R_EntradaSaida_225_7_22()) return true;
+    if (jj_3R_EntradaSaida_224_7_22()) return true;
     }
     return false;
   }
 
-  static private boolean jj_3R_EntradaSaida_224_5_21()
+  static private boolean jj_3R_EntradaSaida_223_5_21()
  {
     if (jj_scan_token(PRINT)) return true;
     return false;
   }
 
-  static private boolean jj_3R_DoWhile_217_5_25()
+  static private boolean jj_3R_DoWhile_216_5_25()
  {
     if (jj_scan_token(DO)) return true;
+    return false;
+  }
+
+  static private boolean jj_3R_Atribuicao_169_5_15()
+ {
+    if (jj_scan_token(ID)) return true;
     return false;
   }
 
@@ -652,27 +658,21 @@ public class pingulangCompiler implements pingulangCompilerConstants {
     return false;
   }
 
-  static private boolean jj_3R_Atribuicao_169_5_15()
- {
-    if (jj_scan_token(ID)) return true;
-    return false;
-  }
-
   static private boolean jj_3R_Comando_162_5_13()
  {
-    if (jj_3R_Bloco_232_5_17()) return true;
-    return false;
-  }
-
-  static private boolean jj_3R_Repeticao_190_23_20()
- {
-    if (jj_3R_DoWhile_217_5_25()) return true;
+    if (jj_3R_Bloco_231_5_17()) return true;
     return false;
   }
 
   static private boolean jj_3R_Comando_160_5_12()
  {
-    if (jj_3R_EntradaSaida_224_5_16()) return true;
+    if (jj_3R_EntradaSaida_223_5_16()) return true;
+    return false;
+  }
+
+  static private boolean jj_3R_Repeticao_189_23_20()
+ {
+    if (jj_3R_DoWhile_216_5_25()) return true;
     return false;
   }
 
@@ -684,11 +684,11 @@ public class pingulangCompiler implements pingulangCompilerConstants {
 
   static private boolean jj_3R_Comando_156_5_10()
  {
-    if (jj_3R_Repeticao_190_5_14()) return true;
+    if (jj_3R_Repeticao_189_5_14()) return true;
     return false;
   }
 
-  static private boolean jj_3R_For_202_5_24()
+  static private boolean jj_3R_For_201_5_24()
  {
     if (jj_scan_token(FOR)) return true;
     return false;
@@ -716,39 +716,39 @@ public class pingulangCompiler implements pingulangCompilerConstants {
 
   static private boolean jj_3_1()
  {
-    if (jj_3R_Condicional_182_5_8()) return true;
+    if (jj_3R_Condicional_181_5_8()) return true;
     return false;
   }
 
-  static private boolean jj_3R_Repeticao_190_15_19()
+  static private boolean jj_3R_Repeticao_189_15_19()
  {
-    if (jj_3R_For_202_5_24()) return true;
+    if (jj_3R_For_201_5_24()) return true;
     return false;
   }
 
-  static private boolean jj_3R_While_196_5_23()
+  static private boolean jj_3R_While_195_5_23()
  {
     if (jj_scan_token(WHILE)) return true;
     return false;
   }
 
-  static private boolean jj_3R_Repeticao_190_5_14()
+  static private boolean jj_3R_Repeticao_189_5_14()
  {
     Token xsp;
     xsp = jj_scanpos;
-    if (jj_3R_Repeticao_190_5_18()) {
+    if (jj_3R_Repeticao_189_5_18()) {
     jj_scanpos = xsp;
-    if (jj_3R_Repeticao_190_15_19()) {
+    if (jj_3R_Repeticao_189_15_19()) {
     jj_scanpos = xsp;
-    if (jj_3R_Repeticao_190_23_20()) return true;
+    if (jj_3R_Repeticao_189_23_20()) return true;
     }
     }
     return false;
   }
 
-  static private boolean jj_3R_Repeticao_190_5_18()
+  static private boolean jj_3R_Repeticao_189_5_18()
  {
-    if (jj_3R_While_196_5_23()) return true;
+    if (jj_3R_While_195_5_23()) return true;
     return false;
   }
 
@@ -759,7 +759,7 @@ public class pingulangCompiler implements pingulangCompilerConstants {
     return false;
   }
 
-  static private boolean jj_3R_Bloco_232_5_17()
+  static private boolean jj_3R_Bloco_231_5_17()
  {
     if (jj_scan_token(INICIO_BLOCO)) return true;
     return false;
